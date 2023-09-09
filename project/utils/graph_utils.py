52c4623acf4b6e914e1ea917aaa18c31dbde11ce
Startcev <tozarin@mail.ru>
2023-09-09 17:20:40 +0300

    fixes codestyle
    Signed-off-by: Startcev <tozarin@mail.ru>

--- project/utils/graph_utils.py
+++ project/utils/graph_utils.py
@@ -3,7 +3,8 @@ from networkx import MultiDiGraph, drawing
 from typing import Tuple
 from collections import namedtuple
 
-Info = namedtuple("Info", ['num_of_nodes', 'num_of_edges', 'marks'])
+Info = namedtuple("Info", ["num_of_nodes", "num_of_edges", "marks"])
+
 
 def get_graph(name: str) -> MultiDiGraph:
 
@@ -12,15 +13,20 @@ def get_graph(name: str) -> MultiDiGraph:
 
     return graph
 
+
 def get_info(name: str) -> Info:
 
     graph = get_graph(name)
-    marks = set([i[2]['label'] for i in graph.edges.data()])
+    marks = set([i[2]["label"] for i in graph.edges.data()])
 
     return Info(graph.number_of_nodes(), graph.number_of_edges(), marks)
 
-def gen_labeled_two_cycles_graph(num_nodes: Tuple[int, int], marks: Tuple[str, str]) -> MultiDiGraph:
-    return labeled_two_cycles_graph(num_nodes[0], num_nodes[1], labels=marks)
+
+def gen_labeled_two_cycles_graph(
+    fst_num_nodes: int, snd_num_nodes: int, marks: Tuple[str, str]
+) -> MultiDiGraph:
+    return labeled_two_cycles_graph(fst_num_nodes, snd_num_nodes, labels=marks)
+
 
 def save_as_dot(graph: MultiDiGraph, path: str):
     drawing.nx_pydot.to_pydot(graph).write_raw(path)
