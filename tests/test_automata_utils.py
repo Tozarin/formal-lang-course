4cbe08d19c3f434d3c32851b8882bd4ab42f7f19
Startcev <tozarin@mail.ru>
2023-09-18 00:38:56 +0300

    fixes code style
    Signed-off-by: Startcev <tozarin@mail.ru>

--- tests/test_automata_utils.py
+++ tests/test_automata_utils.py
@@ -4,9 +4,18 @@ from networkx import algorithms, is_isomorphic, MultiDiGraph
 from pyformlang.finite_automaton import FiniteAutomaton
 from pyformlang.regular_expression import Regex
 
-from project.utils.graph_utils import gen_labeled_two_cycles_graph, load_from_dot, get_graph
-from project.utils.automata_utils import gen_min_dfa_by_reg, gen_nfa_by_graph, AutomataExepction
+from project.utils.graph_utils import (
+    gen_labeled_two_cycles_graph,
+    load_from_dot,
+    get_graph,
+)
+from project.utils.automata_utils import (
+    gen_min_dfa_by_reg,
+    gen_nfa_by_graph,
+    AutomataExepction,
+)
 from common_info import path_to_results, reg_test, gen_auto_from_graph_test
+
 
 def is_isomorphic_fa_and_graph(fa: FiniteAutomaton, gr: MultiDiGraph) -> bool:
 
@@ -25,17 +34,20 @@ def is_isomorphic_fa_and_graph(fa: FiniteAutomaton, gr: MultiDiGraph) -> bool:
 
     return is_isomorphic(gr, fa_graph, edge_match=helper)
 
+
 def test_gen_min_dfa_by_reg():
     for reg, path in reg_test:
         fa = gen_min_dfa_by_reg(Regex(reg))
         gr = load_from_dot(path_to_results + path)
         assert is_isomorphic_fa_and_graph(fa, gr)
-    
+
 
 def test_fail_gen_nfa_by_graph():
 
     count_of_nodes = 10
-    gr = gen_labeled_two_cycles_graph(count_of_nodes // 2, count_of_nodes // 2, ("a", "b"))
+    gr = gen_labeled_two_cycles_graph(
+        count_of_nodes // 2, count_of_nodes // 2, ("a", "b")
+    )
 
     ss = set(gr)
     ss.add(count_of_nodes + 1)
@@ -52,16 +64,19 @@ def test_fail_gen_nfa_by_graph():
     except AutomataExepction:
         assert True
 
+
 def test_gen_nfa_by_graph():
     for path in gen_auto_from_graph_test:
         gr = load_from_dot(path_to_results + path)
         fa = gen_nfa_by_graph(gr)
         assert is_isomorphic_fa_and_graph(fa, gr)
 
+
 def test_gen_nfa_by_remoted_graph():
     gr = get_graph("skos")
     fa = gen_nfa_by_graph(gr)
     assert is_isomorphic_fa_and_graph(fa, gr)
+
 
 def test_gen_nfa_by_two_cycles_graph():
     gr = gen_labeled_two_cycles_graph(2, 2, ("a", "b"))
