2006f1b7c71eba335812d9bc49a136e9677ffd6a
Startcev <tozarin@mail.ru>
2023-09-21 16:42:24 +0300

    adds some tests
    Signed-off-by: Startcev <tozarin@mail.ru>

---
+++ tests/test_bin_matrix_utils.py
@@ -0,0 +1,16 @@
+import pytest
+
+from pyformlang.finite_automaton import NondeterministicFiniteAutomaton, State
+
+from project.utils.bin_matrix_utils import build_bm_by_nfa, build_nfa_by_bm
+
+def test_build_empty():
+    
+    nfa = NondeterministicFiniteAutomaton()
+    assert build_nfa_by_bm(build_bm_by_nfa).is_empty()
+
+def test_build_bm_by_nfa():
+    assert True
+
+def test_build_nfa_by_bm():
+    assert True
