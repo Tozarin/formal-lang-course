d827d42bb7de5c24ebad70730935f2545a514488
Startcev <tozarin@mail.ru>
2023-09-21 16:45:59 +0300

    fixes test
    Signed-off-by: Startcev <tozarin@mail.ru>

--- tests/test_bin_matrix_utils.py
+++ tests/test_bin_matrix_utils.py
@@ -8,7 +8,7 @@ from project.utils.bin_matrix_utils import build_bm_by_nfa, build_nfa_by_bm
 def test_build_empty():
 
     nfa = NondeterministicFiniteAutomaton()
-    assert build_nfa_by_bm(build_bm_by_nfa).is_empty()
+    assert build_nfa_by_bm(build_bm_by_nfa(nfa)).is_empty()
 
 
 def test_build_bm_by_nfa():
