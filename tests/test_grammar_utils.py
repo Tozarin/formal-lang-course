a874785c29496446425a5bd25a31e5efcb8ab2c0
Startcev Matvey <tozarin@mail.ru>
2023-10-20 13:04:48 +0300

    fixes code style
    Signed-off-by: Startcev Matvey <tozarin@mail.ru>

--- tests/test_grammar_utils.py
+++ tests/test_grammar_utils.py
@@ -1,23 +1,49 @@
 from pyformlang.cfg import CFG, Variable
 
-from project.utils.grammar_utils import contex_free_to_weak_chomsky_form, read_contex_free_grammar_from_file
-from common_info import path_to_grammars ,contex_free_and_weak_chomsky_forms, names_of_grammar_files
+from project.utils.grammar_utils import (
+    contex_free_to_weak_chomsky_form,
+    read_contex_free_grammar_from_file,
+)
+from common_info import (
+    path_to_grammars,
+    contex_free_and_weak_chomsky_forms,
+    names_of_grammar_files,
+)
+
 
 def test_contex_free_to_weak_chomsky_form():
-    
-    for contex_free_form, contex_free_form_starting_nonterminal, expected_grammar, expected_grammar_starting_symbol in contex_free_and_weak_chomsky_forms:
-        weak_chomsky_form = contex_free_to_weak_chomsky_form(contex_free_form, contex_free_form_starting_nonterminal)
-        expected_grammar = CFG.from_text(expected_grammar, Variable(expected_grammar_starting_symbol))
+
+    for (
+        contex_free_form,
+        contex_free_form_starting_nonterminal,
+        expected_grammar,
+        expected_grammar_starting_symbol,
+    ) in contex_free_and_weak_chomsky_forms:
+        weak_chomsky_form = contex_free_to_weak_chomsky_form(
+            contex_free_form, contex_free_form_starting_nonterminal
+        )
+        expected_grammar = CFG.from_text(
+            expected_grammar, Variable(expected_grammar_starting_symbol)
+        )
 
         assert weak_chomsky_form.start_symbol == expected_grammar.start_symbol
         assert weak_chomsky_form.productions == expected_grammar.productions
 
+
 def test_read_contex_free_grammar_from_file():
 
-    for name_of_file, starting_symbol, expected_grammar, expected_starting_symbol in names_of_grammar_files:
-        expected_grammar = CFG.from_text(expected_grammar, Variable(expected_starting_symbol))
-        contex_free_grammar = read_contex_free_grammar_from_file(path_to_grammars + name_of_file, starting_symbol)
+    for (
+        name_of_file,
+        starting_symbol,
+        expected_grammar,
+        expected_starting_symbol,
+    ) in names_of_grammar_files:
+        expected_grammar = CFG.from_text(
+            expected_grammar, Variable(expected_starting_symbol)
+        )
+        contex_free_grammar = read_contex_free_grammar_from_file(
+            path_to_grammars + name_of_file, starting_symbol
+        )
 
         assert contex_free_grammar.start_symbol == expected_grammar.start_symbol
         assert contex_free_grammar.productions == expected_grammar.productions
-
