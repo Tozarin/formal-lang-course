d2a51820bdc08448cb86af50019ef525967af1bc
Startcev <tozarin@mail.ru>
2023-10-27 20:07:50 +0300

    adds tests
    Signed-off-by: Startcev <tozarin@mail.ru>

--- tests/test_cfpq.py
+++ tests/test_cfpq.py
@@ -5,6 +5,8 @@ from pyformlang.cfg import Variable
 from project.grammar.cfpq import (
     helling_constrained_transitive_closure,
     helling_request,
+    matrix_constrained_transitive_closure,
+    matrix_request,
 )
 from project.utils.graph_utils import load_from_dot
 from project.utils.grammar_utils import read_contex_free_grammar_from_file
@@ -39,6 +41,29 @@ def test_helling_constrained_transitive_closure():
         )
 
 
+def test_matrix_constrained_transitive_closure():
+
+    for (
+        test_graph_name,
+        test_grammar_name,
+        expected_answer,
+    ) in constrained_transitive_closure_examples:
+        test_graph = load_from_dot(path_to_graphs + test_graph_name)
+        test_grammar = read_contex_free_grammar_from_file(
+            path_to_grammars + test_grammar_name
+        )
+
+        expected_answer = {
+            (start_node, Variable(variable), end_node)
+            for start_node, variable, end_node in expected_answer
+        }
+
+        assert (
+            matrix_constrained_transitive_closure(test_graph, test_grammar)
+            == expected_answer
+        )
+
+
 def test_helling_request():
 
     for (
@@ -56,6 +81,33 @@ def test_helling_request():
 
         assert (
             helling_request(
+                test_graph,
+                test_grammar,
+                starting_vertices,
+                final_vertices,
+                starting_symbol,
+            )
+            == expected_answer
+        )
+
+
+def test_matrix_request():
+
+    for (
+        test_graph_name,
+        starting_vertices,
+        final_vertices,
+        starting_symbol,
+        test_grammar_name,
+        expected_answer,
+    ) in helling_examples:
+        test_graph = load_from_dot(path_to_graphs + test_graph_name)
+        test_grammar = read_contex_free_grammar_from_file(
+            path_to_grammars + test_grammar_name
+        )
+
+        assert (
+            matrix_request(
                 test_graph,
                 test_grammar,
                 starting_vertices,
